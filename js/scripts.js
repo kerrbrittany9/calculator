77674560c93b437410f4d8128d6e3a5febe7332b
Kaden Shorf <kaden@>
2017-05-30 14:56:31 -0700

    add JS command for BMI calculator to include **

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-var add = function (number1, number2){return number1 + number2;
+var divide = function (number1, number2){return number2 / number1;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-alert(add(number1, number2));
+var number1 = parseInt(prompt("Enter weight in kg"));
+var number2 = parseInt(prompt("Enter height in m"));
+  number2 ** 2;
+var result = divide(number2, number1);
+alert(result);
