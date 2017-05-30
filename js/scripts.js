b3649d2dd1ce36f2daca35afbbc48d97305cfcf8
Kaden Shorf <kaden@>
2017-05-30 15:29:52 -0700

    add brackets around var userTemp and delete par() at end of code

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var changeToCel = function (Fahrenheit)
+var changeToCel = function (Fahrenheit)  {
 var userTemp = (prompt("Enter Fahrenheit"));
-return(alert(userTemp - 32) * (5/9)) );
-
+return(alert(userTemp - 32) * (5/9));
+};
 changeToCel();
