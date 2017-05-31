c5007786b01c900f8aec97581ac0735907340fb3
Kaden Shorf <kaden@>
2017-05-30 17:25:43 -0700

    make walrus showing/hidden a generic ID #initially...

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 
 $(function() {
-  $("p").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+  $(".clickable").click(function() {
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
   });
 })
